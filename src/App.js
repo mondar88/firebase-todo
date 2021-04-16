60ba1e60164701ed150f9533a6bc304251a6d114
mondar88 <52878910+mondar88@users.noreply.github.com>
2021-04-16 15:20:54 +0530

    feat: authentication set up with sign in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import "./App.css";
 import Todos from "./Todos";
 
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
+import firebase from "firebase";
 
-const sighInWithGoogle = () => {};
+
+const sighInWithGoogle = () => auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
   
 
 const SignIn = () => (
@@ -12,7 +16,7 @@ const SignIn = () => (
 );
 
 const App = () => {
-  const user = "";
+  const [user] = useAuthState(auth);
 
   return user ? <Todos /> : <SignIn />;
 };
